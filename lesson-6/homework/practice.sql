cbe18e59022c45f0f98bcc6bb33afb8cea434fa0
Oybek64 <akhmedoff101@gmail.com>
2025-07-04 06:35:57 +0500

    Create practice.sql

---
+++ lesson-6/homework/practice.sql
@@ -0,0 +1,12 @@
+1. SELECT DISTINCT
+   LEAST(col1, col2) AS col1,
+   GREATEST(col1, col2) AS col2
+   FROM InputTbl;
+
+2. SELECT * FROM TestMultipleZero
+   WHERE NOT (A = 0 AND B = 0 AND C = 0 AND D = 0);
+
+3. SELECT * FROM section1
+   WHERE ID%2=1;
+
+4. 
