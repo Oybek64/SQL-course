a7d993cc5235228cbc70866f41c287699767c4dd
Oybek64 <akhmedoff101@gmail.com>
2025-07-04 09:51:52 +0500

    Update practice.sql

--- lesson-6/homework/practice.sql
+++ lesson-6/homework/practice.sql
@@ -9,4 +9,16 @@
 3. SELECT * FROM section1
    WHERE ID%2=1;
 
-4. 
+4. SELECT *  FROM section1
+   WHERE id = (SELECT MIN(id) FROM section1);
+
+5. SELECT * FROM section1 
+   WHERE id = (SELECT MAX(ID) FROM section1);
+
+6. SELECT * FROM section1
+   WHERE name LIKE 'B%';
+
+7. SELECT *
+   FROM ProductCodes
+   WHERE Code LIKE '%/_%' ESCAPE '/';
+        
