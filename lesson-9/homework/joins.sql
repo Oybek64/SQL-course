23c1d3274fe88fa8252550b3edc868376bac61b3
Oybek64 <akhmedoff101@gmail.com>
2025-07-13 23:28:22 +0500

    Update joins.sql

--- lesson-9/homework/joins.sql
+++ lesson-9/homework/joins.sql
@@ -15,7 +15,9 @@
    INNER JOIN
    Orders ON Customers.CustomerID = Orders.CustomerID;
 
-5. 
+5. SELECT *
+   FROM Students
+   CROSS JOIN Courses;
 
 6. SELECT Products.ProductName, Orders.OrderID
    FROM Products
@@ -26,7 +28,9 @@
    INNER JOIN Departments
    ON Employees.DepartmentID = Departments.DepartmentID;
 
-8.
+8. SELECT Students.StudentID, Students.Name, Enrollments.CourseID
+   FROM Students
+   INNER JOIN Enrollments ON Students.StudentID = Enrollments.StudentID;
 
 9. SELECT Orders.OrderID, Payments.PaymentID, Payments.PaymentDate, Paymentmethod
    FROM Orders
@@ -55,10 +59,95 @@
     ON Customers.CustomerID = Sales.CustomerID
     WHERE Sales.SaleAmount >= 500;
 
-14. 
+14. SELECT Students.Name, Courses.Coursename
+    FROM Enrollments
+    INNER JOIN Students ON Students.StudentID = Enrollments.StudentID
+    INNER JOIN Courses ON Courses.CourseID = Enrollments.CourseID;
+
+15. SELECT ProductName, SupplierName
+    FROM Products
+    INNER JOIN Suppliers
+    ON Products.SupplierID = Suppliers.SupplierID
+    WHERE Suppliers.SupplierName LIKE '%Tech%';
+
+16. SELECT Orders.OrderID, TotalAmount, PaymentID, Amount AS PaymentAmount
+    FROM Orders
+    INNER JOIN Payments
+    ON Orders.OrderID = Payments.OrderID
+    WHERE Payments.Amount < Orders.TotalAmount;
+
+17. SELECT Employees.EmployeeID, Employees.Name, Departments.DepartmentName
+    FROM Employees
+    INNER JOIN Departments ON Employees.DepartmentID = Departments.DepartmentID;
+
+18. SELECT Products.ProductName, Categories.CategoryName
+    FROM Products
+    INNER JOIN Categories ON Products.Category = Categories.CategoryName
+    WHERE CategoryName = 'Electronics' OR CategoryName = 'Furniture';
+
+19. SELECT Sales.SaleID, Customers.FirstName, Customers.LastName, Customers.Country
+    FROM Sales
+    INNER JOIN Customers ON Sales.CustomerID = Customers.CustomerID
+    WHERE Sustomers.Country = 'USA';
+
+20.SELECT Customers.CustomerID, Customers.FirstName, Customers.LastName, Orders.OrderID, Customers.Country, Orders.TotalAmount
+   FROM Customers
+   INNER JOIN Orders ON Orders.CustomerID = Orders.CustomerID
+   WHERE Customers.Country = 'Germany' AND Orders.TotalAmount > 100;
+
+21. SELECT 
+      E1.EmployeeID AS Employee1_ID,
+      E1.Name AS Employee1_Name,
+      E2.EmployeeID AS Employee2_ID,
+      E2.Name AS Employee2_Name
+    FROM 
+      Employees E1
+    INNER JOIN 
+      Employees E2 ON E1.EmployeeID < E2.EmployeeID
+    WHERE 
+      E1.DepartmentID <> E2.DepartmentID;
+
+22.SELECT Orders.OrderID, 
+          Products.ProductName, 
+          Orders.Quantity, 
+          Products.Price, 
+          Payments.PaymentID, 
+          Payments.PaymnetDate, 
+          Payments.Amount, 
+          Payments.PaymentMethod
+  FROM Orders
+  INNER JOIN Payments ON Payments.OrderID = Orders.OrderID
+  INNER JOIN Products ON Products.ProductID = Orders.ProductID
+  WHERE Payments.Amount <> (Orders.Quantity * Products.Price);
 
 
+23. SELECT Students.StudentID, Students.Name, Enrollments.CourseID
+    FROM Students
+    LEFT JOIN Enrollments ON Students.StudentID = Enrollments.StudentID; 
 
+24. SELECT 
+     M.EmployeeID AS ManagerID,
+     M.Name AS Manager_Name,
+     M.Salary AS Manager_Salary,
+     E.EmployeeID AS EmpID,
+     E.Name AS Emp_Name,
+     E.Salary AS Emp_Salary
+    FROM Employees M
+    JOIN Employees E ON M.EmployeeID = E.ManagerID
+    WHERE M.Salary <= E.Salary;
+
+25. 
+SELECT 
+    Customers.CustomerID,
+    Customers.firstName,
+    Orders.OrderID,
+    Orders.OrderDate
+FROM 
+    Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+LEFT JOIN Payments ON Orders.OrderID = Payments.OrderID
+WHERE 
+    Payments.PaymentID IS NULL;
 
 
 
