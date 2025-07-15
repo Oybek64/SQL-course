df13368986f1d5aca31a918e3aa49590fec2b55e
Oybek64 <akhmedoff101@gmail.com>
2025-07-15 07:43:40 +0500

    Update joins2.sql

--- lesson-10/homework/joins2.sql
+++ lesson-10/homework/joins2.sql
@@ -25,7 +25,66 @@
    FROM employees e1
    JOIN employees e2 ON e1.managerid = e2.employeeid; 
 
-7. 
+7. SELECT Students.Name, Courses.CourseName
+   FROM Enrollments
+   INNER JOIN Students ON Enrollments.StudentID = Students.StudentID
+   INNER JOIN Courses ON Enrollments.CourseID = Courses.CourseID
+   WHERE CourseName = 'Math 101';
+
+8. SELECT Customers.FirstName, Customers.Lastname, Orders.Quantity
+   FROM Customers
+   INNER JOIN Orders ON Orders.CustomerID = Customers.CustomerID
+   WHERE Quantity > 3;
+
+9. SELECT E.Name, D.DepartmentName
+   FROM Employees E 
+   INNER JOIN Departments D ON D.DepartmentID = E.DepartmentID
+   WHERE D.DepartmentName = 'Human Resources';
+
+10. SELECT D.DepartmentName, COUNT(E.EmployeeID) AS EmployeeCount
+    FROM Employees E
+    INNER JOIN Departments D ON E.DepartmentID = D.DepartmentID
+    GROUP BY D.DepartmentName
+    HAVING COUNT(E.EmployeeID) > 5;
+
+11. SELECT 
+      p.ProductID,
+      p.ProductName,
+	 FROM 
+      Products p
+    LEFT JOIN 
+      Sales s ON p.ProductID = s.ProductID
+    WHERE 
+      s.ProductID IS NULL;
+
+12. SELECT C.FirstName, 
+           C.LastName, 
+           COUNT(O.OrderID) AS TotalOrders
+    FROM Customers C
+    INNER JOIN Orders O ON C.CustomerId = O.CustomerID
+    GROUP BY C.FirstName, 
+             C.LastName
+    HAVING COUNT(O.OrderID) >= 1;
+
+13. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
