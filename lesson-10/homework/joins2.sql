96663af40eefacde4d2a444bc2f7f602b8e16388
Oybek64 <akhmedoff101@gmail.com>
2025-07-17 07:27:21 +0500

    Update joins2.sql

--- lesson-10/homework/joins2.sql
+++ lesson-10/homework/joins2.sql
@@ -70,7 +70,12 @@
     FROM Employees E
     INNER JOIN Departments D ON E.DepartmentID = D.DepartmentID;
 
-14.
+14. SELECT E1.NAME AS EMPLOYEE1,
+       E2.NAME AS EMPLOYEE2,
+	   E1.MANAGERID
+	   FROM EMPLOYEES E1
+	   INNER JOIN EMPLOYEES E2 ON E1.MANAGERID = E2.MANAGERID
+	   WHERE E1.EMPLOYEEID < E2.EMPLOYEEID
 
 15. SELECT Orders.OrderID, Orders.OrderDate, Customers.FirstName, Customers.LastName
     FROM Orders
@@ -103,7 +108,56 @@
         WHERE DepartmentID = E.DepartmentID
     );
 
-20. 
+20. select Orders.OrderID, Orders.OrderDate
+    from Orders
+    left join Payments ON Orders.OrderID = Payments.OrderID
+    where OrderDate > '2020-01-01' and Payments.PaymentID is null;
+
+21. select ProductID, ProductName
+    from Products
+    left join Categories on Products.Category = Categories.CategoryName 
+    where CategoryName is null
+
+
+ 22. SELECT E1.name as Employee1,
+       E2.name as Employee2,
+	   E1.Managerid as Managerid,
+	   E1.Salary as Salary1,
+	   E2.Salary as Salary2
+	   from employees E1
+	   inner join employees E2 on E1.managerid = e2.managerid
+	   and e1.employeeid < e2.employeeid
+	   where e1.salary > 60000 and  e2.salary > 60000
+
+23. select E.Name as EmployeeName, D.DepartmentName
+    from Employees E
+    inner join Departments D on E.DepartmentID = D.DepartmentID
+    where D.DepartmentName LIKE 'M%';
+
+
+24. select SaleID, ProductName, SaleAmount
+    from Sales
+    inner join Products on Sales.ProductID = Products.ProductID
+    where SaleAmount > 500;
+
+25. SELECT S.StudentID, S.Name
+    FROM Students S
+    WHERE S.StudentID NOT IN (
+    SELECT E.StudentID
+    FROM Enrollments E
+    INNER JOIN Courses C ON E.CourseID = C.CourseID
+    WHERE C.CourseName = 'Math 101'
+);
+
+26. select O.OrderID, O.OrderDate, P.PaymentID
+    from Orders O
+    inner join Payments P on O.OrderID = P.OrderID
+    where P.PaymentID is null;
+
+27. select ProductID, ProductName, CategoryName
+    from Products
+    inner join Categories on Products.Category = Categories.CategoryName
+    where CategoryName IN ('Electronics', 'Furniture');
 
 
 
