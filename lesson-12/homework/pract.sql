aa9219aa01a081cb3141bc4a8971c6096613a6ca
Oybek64 <akhmedoff101@gmail.com>
2025-07-23 17:42:19 +0500

    Update pract.sql

--- lesson-12/homework/pract.sql
+++ lesson-12/homework/pract.sql
@@ -14,4 +14,25 @@
 
 4. 
 
+
+
+5.select  girls.parentname as g_parentname, girls.name as g_name, boys.parentname as b_parentname
+from girls
+left join boys on girls.parentname = boys.parentname
+where boys.parentname is null
+
+6. 
+
+7.select cart1.item as Item_cart_1, cart2.item as Item_cart_2
+  from Cart1
+  full join cart2
+  on cart1.item = cart2.item
+
+8. SELECT customers.name
+FROM Customers
+LEFT JOIN Orders ON customers.id = orders.customerId
+WHERE orders.customerId IS NULL;
+
+9.
+
  
